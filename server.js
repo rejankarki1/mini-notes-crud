0fb74ee43b0e4a3b979e29880d61d41e5af04685
Demo for Git <rejankkarkiv2@gmail.com>
2026-05-19 18:53:24 -0500

    Built mini notes CRUD app using Express and EJS

---
+++ server.js
@@ -0,0 +1,95 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import methodOverride from "method-override";
+import { v4 as uuidv4 } from "uuid";
+
+const app = express();
+const port = 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+let notes = [
+    {
+        id: uuidv4(),
+        title: "Learn Express",
+        description: "Practice REST CRUD routes."
+    },
+    {
+        id: uuidv4(),
+        title: "Mini Notes Project",
+        description: "Build notes app step by step."
+    }
+];
+
+app.get("/", (req, res) => {
+    res.redirect("/notes");
+});
+
+app.get("/notes", (req, res) => {
+    res.render("index", { notes });
+});
+
+app.get("/notes/new", (req, res) => {
+    res.render("new");
+});
+
+app.post("/notes", (req, res) => {
+    let { title, description } = req.body;
+
+    let newNote = {
+        id: uuidv4(),
+        title,
+        description
+    };
+
+    notes.push(newNote);
+
+    res.redirect("/notes");
+});
+
+app.get("/notes/:id", (req, res) => {
+    let { id } = req.params;
+    let note = notes.find((note) => id === note.id);
+
+    res.render("show", { note });
+});
+
+app.get("/notes/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let note = notes.find((note) => id === note.id);
+
+    res.render("edit", { note });
+});
+
+app.patch("/notes/:id", (req, res) => {
+    let { id } = req.params;
+    let { title, description } = req.body;
+
+    let note = notes.find((note) => id === note.id);
+
+    note.title = title;
+    note.description = description;
+
+    res.redirect(`/notes/${id}`);
+});
+
+app.delete("/notes/:id", (req, res) => {
+    let { id } = req.params;
+
+    notes = notes.filter((note) => id !== note.id);
+
+    res.redirect("/notes");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
